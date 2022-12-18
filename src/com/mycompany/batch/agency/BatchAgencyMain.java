6100816de89d639a6a38e298d8fa9f29c99ddbd0
정영훈 <yunghun0815@gmail.com>
2022-12-18 15:24:48 +0900

    FIX : 확장자에 따른 분기처리

--- src/com/mycompany/batch/agency/BatchAgencyMain.java
+++ src/com/mycompany/batch/agency/BatchAgencyMain.java
@@ -1,10 +1,13 @@
 package com.mycompany.batch.agency;
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
+import java.sql.Date;
+
+import org.json.JSONObject;
 
 public class BatchAgencyMain {
 	public static void main(String[] args) throws Exception {
@@ -16,8 +19,28 @@ public class BatchAgencyMain {
 			DataInputStream dis = new DataInputStream(socket.getInputStream());
 			DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
 			
+			
 			String path = dis.readUTF();
-			System.out.println("path: " + path);
+			int lastDot = path.lastIndexOf(".");
+			
+			//확장자
+			String extension = path.substring(lastDot +1);
+			System.out.println(extension);
+			if(extension.equals("jar")) {
+				path = "java -jar " + path;
+			}
+			Process process = Runtime.getRuntime().exec(path);
+			BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			
+			String line = null;
+			StringBuffer sb = new StringBuffer();
+			while((line = br.readLine()) != null ) {
+				sb.append(line);
+			}
+			System.out.println("실행결과 : " + sb.toString());
+			dos.writeUTF(sb.toString());
+			dos.flush();
+			//System.out.println("path: " + path);
 			
 			dis.close();
 			dos.close();
