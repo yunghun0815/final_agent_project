9cd0f05d1f9f1e3150fc8aae61b070ed66f085f1
정영훈 <yunghun0815@gmail.com>
2022-12-20 11:11:26 +0900

    FIX : JSONObject 임포트 제

--- src/com/mycompany/batch/agency/BatchAgencyMain.java
+++ src/com/mycompany/batch/agency/BatchAgencyMain.java
@@ -5,9 +5,7 @@ import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.sql.Date;
 
-import org.json.JSONObject;
 
 public class BatchAgencyMain {
 	public static void main(String[] args) throws Exception {
